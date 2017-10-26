ebeeaf1d077ff34df022c97482264a24fc9c2164
kmcc063 <kmcc063@aucklanduni.ac.nz>
2017-10-26 19:17:38 +1300

    Project upload

---
+++ Products-2017-1-1f1/Assets/Town.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Town : MonoBehaviour {
+
+    //List all the columns of your table here in the same format as below.
+    public string ID { get; set; }
+    public string TownId { get; set; }
+    public string TownName { get; set; }
+    public float Latitude { get; set; }
+    public float Altitude { get; set; }
+    public float Longitude { get; set; }
+    public float Size { get; set; }
+    public string Symbol { get; set; }
+
+}
